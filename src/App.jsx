7bf84026b47e66bad873f3d111a00797869f2abf
Md asik <asikthe1st@gmail.com>
2025-10-17 00:25:21 +0600

    feat: basic layout done. now for a bit of a logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 function App() {
-  return <>
-  hello world</>;
+  return <div className="w-screen h-screen bg-blue-500 flex items-center justify-center">
+    <div className="buttons flex gap-1 items-center">
+      <button className="bg-white w-8 h-8 flex items-center justify-center border-2 border-gray-400 uppercase rounded">v</button>
+      <button className="bg-white w-8 h-8 flex items-center justify-center border-2 border-gray-400 uppercase rounded">h</button>
+      <button className="bg-white w-8 h-8 flex items-center justify-center border-2 border-gray-400 uppercase rounded">-</button>
+    </div>
+  </div>;
 }
 
 export default App;
